5fb8d06f7277df74487bb14d6a3368d96069ab36
Daniel <dany_c@hotmail.com>
2017-05-09 10:49:22 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,101 @@
+/**
+ * Created by danielkopel on 02/05/2017.
+ */
+
+let myGamepadApi = (function(){
+    let characterPosition_H = 0;
+    let characterPosition_V = 0;
+    let gameCharacters = [];
+    let isGameLoopRunning = false;
+    const mainContainer = document.querySelector('body');
+
+    function appendCharacterBox() {
+        const characterBox = document.createElement('div');
+        characterBox.classList.add('character-box');
+        characterBox.id = `game_character_${gameCharacters.length + 1}`;
+        gameCharacters.push(characterBox);
+
+        mainContainer.appendChild(characterBox);
+    }
+
+    function appendLaser(){
+        const laserInitTopPosition = characterPosition_V + 5;
+        const laserInitLeftPosition = gameCharacters[0].offsetWidth + 5;
+        console.log(`T: ${laserInitTopPosition}, L: ${laserInitLeftPosition}`);
+        const laserBox = document.createElement('div');
+        laserBox.classList.add('laser-box');
+        laserBox.style.top = `${laserInitTopPosition}px`;
+        laserBox.style.left = `${laserInitLeftPosition}px`;
+
+        mainContainer.appendChild(laserBox);
+    }
+
+    function fireLaser(){
+        console.log('PONY!');
+        const ponyLaser = appendLaser();
+    }
+
+    function setEvents(){
+        window.addEventListener('gamepadconnected', (evt) => {
+            console.log('CONNECTED EVENT: ', evt);
+            console.log('Gamepad Index: ', evt.gamepad.index);
+            console.log('Gamepad ID: ', evt.gamepad.id);
+            //debugger;
+
+            alert('Gamepad Connected! Cherio!');
+
+            if(!isGameLoopRunning){
+                isGameLoopRunning = true;
+                gameLoop();
+            }
+        });
+
+        window.addEventListener('gamepaddisconnected', () => {
+            alert('Gamepad Disconnected!!');
+        });
+    }
+
+    function gameLoop(){
+        if(navigator.getGamepads()[0].buttons[15].pressed){
+            characterPosition_H++;
+        }
+        else if(navigator.getGamepads()[0].buttons[14].pressed){
+            characterPosition_H--;
+        }
+
+        if(navigator.getGamepads()[0].buttons[12].pressed){
+            characterPosition_V--;
+        }
+        else if(navigator.getGamepads()[0].buttons[13].pressed){
+            characterPosition_V++;
+        }
+
+        if(navigator.getGamepads()[0].buttons[0].pressed){
+            console.log('Pinky Laser!');
+            gameCharacters[0].classList.add('fire');
+            fireLaser();
+        }
+        else{
+            gameCharacters[0].classList.remove('fire');
+        }
+
+        gameCharacters[0].style.left = characterPosition_H + 'px';
+        gameCharacters[0].style.top = characterPosition_V + 'px';
+
+        requestAnimationFrame(gameLoop);
+    }
+
+    function init(){
+        appendCharacterBox();
+        setEvents();
+        gameLoop();
+
+
+    }
+
+    return {
+        init
+    };
+}());
+
+myGamepadApi.init();
